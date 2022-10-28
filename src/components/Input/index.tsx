890c411f7c5fd6eab382dda98bbddb1bdc300c88
pedro-pgpr <pedro-pgpr@defensoria.go.def.br>
2022-10-28 17:51:10 -0300

    Finished Refactoring

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,21 @@ import {
   useRef,
   useState,
   useCallback,
+  ElementType,
+  InputHTMLAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  Icon?: ElementType;
+}
+
+export const Input = ({ name, Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<any>();
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -49,5 +56,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
